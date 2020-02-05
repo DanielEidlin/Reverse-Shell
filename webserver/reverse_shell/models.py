4d1935506e98a09cebd12b7a4bbbaf12b1fc127d
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-02-05 08:19:09 +0200

    Change computer_name field to blank=True

--- webserver/reverse_shell/models.py
+++ webserver/reverse_shell/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Attacker(models.Model):
     ip = models.GenericIPAddressField()
     port = models.IntegerField()
-    computer_name = models.CharField(max_length=200, default='Anonymous Computer')
+    computer_name = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.computer_name + ' ' + self.ip + ' ' + self.port
@@ -13,7 +13,7 @@ class Attacker(models.Model):
 class Victim(models.Model):
     ip = models.GenericIPAddressField()
     port = models.IntegerField()
-    computer_name = models.CharField(max_length=200, default='Anonymous Computer')
+    computer_name = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.computer_name + ' ' + self.ip + ' ' + self.port
