8d137de0efd6ebc7cc9790f17f58f6a3301dfe20
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-02-06 11:07:47 +0200

    Add available_victims view

--- webserver/reverse_shell/views.py
+++ webserver/reverse_shell/views.py
@@ -2,6 +2,8 @@ from django.views import View
 from rest_framework import viewsets
 from django.http import HttpResponse
 from .models import Attacker, Victim
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import AttackerSerializer, VictimSerializer
 
 
@@ -24,3 +26,13 @@ class VictimViewSet(viewsets.ModelViewSet):
     """
     queryset = Victim.objects.all()
     serializer_class = VictimSerializer
+
+    @action(detail=False, methods=['get'])
+    def available_victims(self, request):
+        """
+        Returns all the logged in victims.
+        :return: Serialized victims that are logged in.
+        """
+        available_victims = Victim.objects.filter(logged_in=True)
+        serialized_victims = VictimSerializer(available_victims, many=True)
+        return Response(serialized_victims.data)
