63f96998c586c36be26e4627d99e01afc816de86
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-02-29 18:50:28 +0200

    Add victim field to Attacker model

--- webserver/reverse_shell/models.py
+++ webserver/reverse_shell/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 
 
-class Attacker(models.Model):
-    ip = models.GenericIPAddressField()
-    port = models.IntegerField()
-    mac_address = models.CharField(max_length=200, unique=True)
-    computer_name = models.CharField(max_length=200, blank=True)
-
-    def __str__(self):
-        return self.computer_name + ' ' + self.ip + ' ' + self.port
-
-
 class Victim(models.Model):
     ip = models.GenericIPAddressField()
     port = models.IntegerField()
     mac_address = models.CharField(max_length=200, unique=True)
     computer_name = models.CharField(max_length=200, blank=True)
     logged_in = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.computer_name + ' ' + self.ip + ' ' + self.port
+
+
+class Attacker(models.Model):
+    ip = models.GenericIPAddressField()
+    port = models.IntegerField()
+    mac_address = models.CharField(max_length=200, unique=True)
+    computer_name = models.CharField(max_length=200, blank=True)
+    victim = models.OneToOneField(Victim, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.computer_name + ' ' + self.ip + ' ' + self.port
