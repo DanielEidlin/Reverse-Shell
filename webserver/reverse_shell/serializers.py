b8c7e7c36c95750bf42dceac3bd3489862e6f9d5
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-03-01 22:45:35 +0200

    Fix serializers

--- webserver/reverse_shell/serializers.py
+++ webserver/reverse_shell/serializers.py
@@ -5,10 +5,10 @@ from rest_framework import serializers
 class AttackerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attacker
-        fields = ['id', 'ip', 'port', 'computer_name', 'mac_address']
+        fields = ['ip', 'port', 'computer_name', 'mac_address']
 
 
 class VictimSerializer(serializers.ModelSerializer):
     class Meta:
         model = Victim
-        fields = ['id', 'ip', 'port', 'computer_name', 'mac_address', 'logged_in']
+        fields = ['ip', 'port', 'computer_name', 'mac_address', 'logged_in']
